a0c9034ffeef6a5afa5228de5052cbb0eb456449
Safraz Razik <safrazik@gmail.com>
2020-02-25 15:46:13 +0530

    Optimize and add offline support

--- assets/clock-snap/README.txt
+++
@@ -1,3 +0,0 @@
-A Pen created at CodePen.io. You can find this one at http://codepen.io/rodnylobos/pen/KoJxq.
-
- studies on Snap SVG
---
+++ workbox-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "globDirectory": ".",
+  "globPatterns": [
+    "index.html",
+    // "**/*.{css,js,html,jpg}",
+    "prayer-data-*.js",
+    "assets/**/*.{css,js}",
+    "**/backgrounds/*.jpg"
+  ],
+  "swDest": "sw.js"
+};
