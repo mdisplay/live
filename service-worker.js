d71ebc7152ae64abbce7183f9570cfcdda174b5f
Safraz Razik <safrazik@gmail.com>
2021-03-23 20:21:00 +0530

    Update version 1.13.0-65

--- service-worker.js
+++ service-worker.js
@@ -6,7 +6,7 @@ if (workbox) {
   console.log(`Boo! Workbox didn't load 😬`);
 }
 
-var appVersion = '1.12.2-64';
+var appVersion = '1.13.0-65';
 var dataVersion = '6';
 var vendorVersion = '5';
 var bgVersion = '7';
